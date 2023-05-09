ed15fcd7752cd459ec6ed2ff6a442d37763b79cd
daniArbesu <daniel.arbesu.sanchez@gmail.com>
2023-05-09 14:47:46 +0200

    feat: add express configuration

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json({ limit: '100kb' }));
+app.use(express.urlencoded({ extended: false }));
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server Running in port ${PORT}!`);
