7fe5570a0b0473ccf380672162da4f8af159f6c3
daniArbesu <daniel.arbesu.sanchez@gmail.com>
2023-05-09 17:28:29 +0200

    feat: add all seeds

--- src/seed/index.js
+++ src/seed/index.js
@@ -2,13 +2,15 @@
 /* eslint-disable import/extensions */
 import '../config/env.js';
 import '../config/db.js';
-import { Athlete } from '../models/mongo.js';
+import { Athlete, AthleteResults, Game } from '../models/mongo.js';
 import seed from './seed.js';
 
 const main = async () => {
   // populating seed for exercises and routines
   console.log('>> Populating seeds in DB');
   const athletes = await Athlete.insertMany(seed.athletes);
+  const athleteResults = await AthleteResults.insertMany(seed.athleteResult);
+  const olympicGames = await Game.insertMany(seed.games);
   console.log('>> Correctly populated seeds in DB');
 };
 
--- src/seed/seed.js
+++ src/seed/seed.js
@@ -6,7 +6,7 @@ const seed = {
       date_of_birth: '14/04/1990',
       height: 164,
       name: 'Arianna',
-      photo_id: 1,
+      photo_url: '#',
       surname: 'Fontana',
       weight: 63
     },
@@ -16,7 +16,7 @@ const seed = {
       date_of_birth: '16/02/1969',
       height: 175,
       name: 'Fermín',
-      photo_id: 10,
+      photo_url: '#',
       surname: 'Cacho',
       weight: 65
     },
@@ -26,7 +26,7 @@ const seed = {
       date_of_birth: '15/04/1957',
       height: 165,
       name: 'Evelyn',
-      photo_id: 11,
+      photo_url: '#',
       surname: 'Ashford',
       weight: 52
     },
@@ -36,7 +36,7 @@ const seed = {
       date_of_birth: '19/11/1966',
       height: 160,
       name: 'Yolanda',
-      photo_id: 12,
+      photo_url: '#',
       surname: 'Gail Devers-Roberts',
       weight: 55
     },
@@ -46,7 +46,7 @@ const seed = {
       date_of_birth: '16/05/1975',
       height: 154,
       name: 'Simon',
-      photo_id: 13,
+      photo_url: '#',
       surname: 'Whitfield',
       weight: 70
     },
@@ -56,7 +56,7 @@ const seed = {
       date_of_birth: '23/12/1975',
       height: 186,
       name: 'Robert',
-      photo_id: 14,
+      photo_url: '#',
       surname: 'Bartko',
       weight: 80
     },
@@ -66,7 +66,7 @@ const seed = {
       date_of_birth: '19/09/1967',
       height: 193,
       name: 'Aleksandr',
-      photo_id: 15,
+      photo_url: '#',
       surname: 'Karelin',
       weight: 99
     },
@@ -76,7 +76,7 @@ const seed = {
       date_of_birth: '14/09/1974',
       height: 176,
       name: 'Hicham',
-      photo_id: 16,
+      photo_url: '#',
       surname: 'El Guerrouj',
       weight: 58
     },
@@ -86,7 +86,7 @@ const seed = {
       date_of_birth: '30/06/1985',
       height: 193,
       name: 'Michael',
-      photo_id: 17,
+      photo_url: '#',
       surname: 'Phelps',
       weight: 91
     },
@@ -96,7 +96,7 @@ const seed = {
       date_of_birth: '06/06/1972',
       height: 176,
       name: 'Noriaki',
-      photo_id: 18,
+      photo_url: '#',
       surname: 'Kasai',
       weight: 60
     },
@@ -106,7 +106,7 @@ const seed = {
       date_of_birth: '10/02/1985',
       height: 169,
       name: 'Lidia',
-      photo_id: 2,
+      photo_url: '#',
       surname: 'Valentin Perez',
       weight: 75
     },
@@ -116,7 +116,7 @@ const seed = {
       date_of_birth: '07/03/1994',
       height: 196,
       name: 'Chase',
-      photo_id: 3,
+      photo_url: '#',
       surname: 'Kalisz',
       weight: 86
     },
@@ -126,7 +126,7 @@ const seed = {
       date_of_birth: '15/05/1987',
       height: 182,
       name: 'Andy',
-      photo_id: 4,
+      photo_url: '#',
       surname: 'Murray',
       weight: 83
     },
@@ -136,7 +136,7 @@ const seed = {
       date_of_birth: '21/08/1986',
       height: 196,
       name: 'Usain',
-      photo_id: 5,
+      photo_url: '#',
       surname: 'Bolt',
       weight: 95
     },
@@ -146,7 +146,7 @@ const seed = {
       date_of_birth: '25/03/1983',
       height: 178,
       name: 'Javier',
-      photo_id: 6,
+      photo_url: '#',
       surname: 'Gomez Noya',
       weight: 69
     },
@@ -156,7 +156,7 @@ const seed = {
       date_of_birth: '10/07/1988',
       height: 176,
       name: 'Sarah',
-      photo_id: 7,
+      photo_url: '#',
       surname: 'Walker',
       weight: 74
     },
@@ -166,7 +166,7 @@ const seed = {
       date_of_birth: '01/07/1961',
       height: 188,
       name: 'Carl',
-      photo_id: 8,
+      photo_url: '#',
       surname: 'Lewis',
       weight: 80
     },
@@ -176,14 +176,381 @@ const seed = {
       date_of_birth: '9/12/1966',
       height: 162,
       name: 'Joaquim',
-      photo_id: 9,
+      photo_url: '#',
       surname: 'Cruz',
       weight: 74
     }
   ],
-  routines: [
-    { _routineid: 1, name: 'Pull #1', _exercises: [1, 2, 3, 4, 5, 6], sets: 3, reps: '8-10' },
-    { _routineid: 2, name: 'Push #1', _exercises: [7, 8, 9, 10, 11, 12], sets: 3, reps: '8-10' }
+  athleteResult: [
+    {
+      _athlete_id: '1',
+      bronze: 1,
+      _game_id: 4,
+      gold: 1,
+      silver: 1
+    },
+    {
+      _athlete_id: '1',
+      bronze: 2,
+      _game_id: 5,
+      gold: 0,
+      silver: 1
+    },
+    {
+      _athlete_id: '1',
+      bronze: 1,
+      _game_id: 6,
+      gold: 0,
+      silver: 0
+    },
+    {
+      _athlete_id: '1',
+      bronze: 1,
+      _game_id: 7,
+      gold: 0,
+      silver: 0
+    },
+    {
+      _athlete_id: '2',
+      bronze: 0,
+      _game_id: 8,
+      gold: 0,
+      silver: 1
+    },
+    {
+      _athlete_id: '2',
+      bronze: 0,
+      _game_id: 3,
+      gold: 1,
+      silver: 0
+    },
+    {
+      _athlete_id: '2',
+      bronze: 1,
+      _game_id: 2,
+      gold: 0,
+      silver: 0
+    },
+    {
+      _athlete_id: '3',
+      bronze: 0,
+      _game_id: 2,
+      gold: 0,
+      silver: 1
+    },
+    {
+      _athlete_id: '4',
+      bronze: 0,
+      _game_id: 3,
+      gold: 1,
+      silver: 1
+    },
+    {
+      _athlete_id: '4',
+      bronze: 0,
+      _game_id: 2,
+      gold: 1,
+      silver: 0
+    },
+    {
+      _athlete_id: '5',
+      bronze: 0,
+      _game_id: 8,
+      gold: 2,
+      silver: 0
+    },
+    {
+      _athlete_id: '5',
+      bronze: 0,
+      _game_id: 3,
+      gold: 3,
+      silver: 0
+    },
+    {
+      _athlete_id: '5',
+      bronze: 0,
+      _game_id: 2,
+      gold: 3,
+      silver: 0
+    },
+    {
+      _athlete_id: '7',
+      bronze: 0,
+      _game_id: 3,
+      gold: 0,
+      silver: 1
+    },
+    {
+      _athlete_id: '8',
+      bronze: 0,
+      _game_id: 16,
+      gold: 4,
+      silver: 0
+    },
+    {
+      _athlete_id: '8',
+      bronze: 0,
+      _game_id: 15,
+      gold: 2,
+      silver: 1
+    },
+    {
+      _athlete_id: '8',
+      bronze: 0,
+      _game_id: 14,
+      gold: 2,
+      silver: 0
+    },
+    {
+      _athlete_id: '8',
+      bronze: 0,
+      _game_id: 13,
+      gold: 1,
+      silver: 0
+    },
+    {
+      _athlete_id: '9',
+      bronze: 0,
+      _game_id: 16,
+      gold: 1,
+      silver: 0
+    },
+    {
+      _athlete_id: '9',
+      bronze: 0,
+      _game_id: 15,
+      gold: 0,
+      silver: 1
+    },
+    {
+      _athlete_id: '10',
+      bronze: 0,
+      _game_id: 13,
+      gold: 0,
+      silver: 1
+    },
+    {
+      _athlete_id: '10',
+      bronze: 0,
+      _game_id: 14,
+      gold: 1,
+      silver: 0
+    },
+    {
+      _athlete_id: '11',
+      bronze: 0,
+      _game_id: 14,
+      gold: 1,
+      silver: 0
+    },
+    {
+      _athlete_id: '11',
+      bronze: 0,
+      _game_id: 15,
+      gold: 1,
+      silver: 1
+    },
+    {
+      _athlete_id: '11',
+      bronze: 0,
+      _game_id: 16,
+      gold: 2,
+      silver: 0
+    },
+    {
+      _athlete_id: '12',
+      bronze: 0,
+      _game_id: 13,
+      gold: 2,
+      silver: 0
+    },
+    {
+      _athlete_id: '12',
+      bronze: 0,
+      _game_id: 14,
+      gold: 1,
+      silver: 0
+    },
+    {
+      _athlete_id: '13',
+      bronze: 0,
+      _game_id: 11,
+      gold: 1,
+      silver: 0
+    },
+    {
+      _athlete_id: '13',
+      bronze: 0,
+      _game_id: 8,
+      gold: 0,
+      silver: 1
+    },
+    {
+      _athlete_id: '14',
+      bronze: 0,
+      _game_id: 11,
+      gold: 2,
+      silver: 0
+    },
+    {
+      _athlete_id: '16',
+      bronze: 0,
+      _game_id: 9,
+      gold: 2,
+      silver: 0
+    },
+    {
+      _athlete_id: '16',
+      bronze: 0,
+      _game_id: 11,
+      gold: 0,
+      silver: 1
+    },
+    {
+      _athlete_id: '15',
+      bronze: 0,
+      _game_id: 11,
+      gold: 0,
+      silver: 1
+    },
+    {
+      _athlete_id: '15',
+      bronze: 0,
+      _game_id: 13,
+      gold: 1,
+      silver: 0
+    },
+    {
+      _athlete_id: '15',
+      bronze: 0,
+      _game_id: 14,
+      gold: 1,
+      silver: 0
+    },
+    {
+      _athlete_id: '15',
+      bronze: 0,
+      _game_id: 15,
+      gold: 1,
+      silver: 0
+    },
+    {
+      _athlete_id: '17',
+      bronze: 0,
+      _game_id: 2,
+      gold: 5,
+      silver: 1
+    },
+    {
+      _athlete_id: '17',
+      bronze: 0,
+      _game_id: 3,
+      gold: 4,
+      silver: 2
+    },
+    {
+      _athlete_id: '17',
+      bronze: 0,
+      _game_id: 8,
+      gold: 8,
+      silver: 0
+    },
+    {
+      _athlete_id: '17',
+      bronze: 2,
+      _game_id: 9,
+      gold: 6,
+      silver: 0
+    },
+    {
+      _athlete_id: '18',
+      bronze: 1,
+      _game_id: 5,
+      gold: 0,
+      silver: 1
+    }
+  ],
+  games: [
+    {
+      city: 'Tokyo',
+      _game_id: 1,
+      year: 2020
+    },
+    {
+      city: 'Rio de Janeiro',
+      _game_id: 2,
+      year: 2016
+    },
+    {
+      city: 'London',
+      _game_id: 3,
+      year: 2012
+    },
+    {
+      city: 'Pyeongchang',
+      _game_id: 4,
+      year: 2018
+    },
+    {
+      city: 'Sochi',
+      _game_id: 5,
+      year: 2014
+    },
+    {
+      city: 'Vancouver',
+      _game_id: 6,
+      year: 2010
+    },
+    {
+      city: 'Turin',
+      _game_id: 7,
+      year: 2006
+    },
+    {
+      city: 'Beijing',
+      _game_id: 8,
+      year: 2008
+    },
+    {
+      city: 'Athens',
+      _game_id: 9,
+      year: 2004
+    },
+    {
+      city: 'Salt Lake City',
+      _game_id: 10,
+      year: 2002
+    },
+    {
+      city: 'Sydney',
+      _game_id: 11,
+      year: 2000
+    },
+    {
+      city: 'Nagano',
+      _game_id: 12,
+      year: 1998
+    },
+    {
+      city: 'Atlanta',
+      _game_id: 13,
+      year: 1996
+    },
+    {
+      city: 'Barcelona',
+      _game_id: 14,
+      year: 1992
+    },
+    {
+      city: 'Seoul',
+      _game_id: 15,
+      year: 1988
+    },
+    {
+      city: 'Los Angeles',
+      _game_id: 16,
+      year: 1984
+    }
   ]
 };
 
