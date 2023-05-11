1ab63bfd8f14708d6ad45562ef03178d0f97b73f
daniArbesu <daniel.arbesu.sanchez@gmail.com>
2023-05-11 17:44:50 +0200

    feat: add nodemon and Athlete Schema

--- src/models/mongo.js
+++ src/models/mongo.js
@@ -1,17 +1,17 @@
 import mongoose from 'mongoose';
 
 const emptySchema = new mongoose.Schema({});
-/* const athleteSchema = new mongoose.Schema({
+const athleteSchema = new mongoose.Schema({
   name: { type: String, required: true },
   surname: { type: String, required: true },
   date_of_birth: String,
   bio: String,
   height: String,
   weight: String,
-  photoURL: String
-}); */
+  photo_url: String
+});
 
 export const User = mongoose.model('User', emptySchema);
-export const Athlete = mongoose.model('Athlete', emptySchema);
+export const Athlete = mongoose.model('Athlete', athleteSchema);
 export const Game = mongoose.model('Game', emptySchema);
 // export const AthleteResults = mongoose.model('AthleteResults', emptySchema);
